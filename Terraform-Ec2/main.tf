0d7d4a06fc13ccf8d7f97d61085ca9bceb08084b
srinath sriramula <sriramulasrinath59@gmail.com>
2025-11-29 11:43:06 +0530

    Added Terraform EC2 module files

---
+++ Terraform-Ec2/main.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "web_server_sg" {
+  name        = "web-server-security-group"
+  description = "Allows HTTP/HTTPS and SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id # Reference to an existing VPC
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH from specific IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Represents all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-server-sg"
+  }
+}
+
+resource "aws_instance" "Test"{
+    ami = data.aws_ami.ami_info
+    instance_type = var.instance_type
+    tags = {
+      Name = "web"
+      Env = "Testing" 
+    }
+}
---
+++ Terraform-Ec2/var.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+    default = ""
+  
+}
